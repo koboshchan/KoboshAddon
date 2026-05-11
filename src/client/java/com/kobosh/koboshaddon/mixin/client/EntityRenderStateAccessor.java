9f2bc1048d673eb345d8f41e821090f13ef37009
koboshchan <lucaszhang1001@gmail.com>
2026-05-10 18:19:03 -0700

    feat: Implement EntityRenderStateAccessor for name tag access and update EntityRendererMixin to use it

---
+++ src/client/java/com/kobosh/koboshaddon/mixin/client/EntityRenderStateAccessor.java
@@ -0,0 +1,16 @@
+package com.kobosh.koboshaddon.mixin.client;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+import net.minecraft.text.Text;
+
+@Mixin(targets = "net.minecraft.class_10017", remap = false)
+public interface EntityRenderStateAccessor
+{
+	@Accessor(value = "field_53337", remap = false)
+	Text koboshaddon$getNameTag();
+
+	@Accessor(value = "field_53337", remap = false)
+	void koboshaddon$setNameTag(Text nameTag);
+}
--- src/client/java/com/kobosh/koboshaddon/mixin/client/EntityRendererMixin.java
+++ src/client/java/com/kobosh/koboshaddon/mixin/client/EntityRendererMixin.java
@@ -1,7 +1,5 @@
 package com.kobosh.koboshaddon.mixin.client;
 
-import java.lang.reflect.Field;
-
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -19,10 +17,6 @@ import net.wurstclient.hack.Hack;
 @Mixin(targets = "net.minecraft.class_897", priority = 500)
 public abstract class EntityRendererMixin<T extends Entity, S extends EntityRenderState>
 {
-	private static final String[] NAME_FIELD_CANDIDATES =
-		{"nameTag", "name", "displayName"};
-	private static Field cachedNameField;
-
 	@Inject(
 		method = "method_62354(Lnet/minecraft/class_1297;Lnet/minecraft/class_10017;F)V",
 		remap = false,
@@ -30,79 +24,17 @@ public abstract class EntityRendererMixin<T extends Entity, S extends EntityRend
 	private void addCoordToDisplayName(T entity, S state, float tickProgress,
 		CallbackInfo ci)
 	{
-		if(!(entity instanceof PlayerEntity player))
+		EntityRenderStateAccessor accessor = (EntityRenderStateAccessor)state;
+		if(accessor.koboshaddon$getNameTag() == null)
 			return;
-
-		Text nameTag = getNameTag(state);
-		if(nameTag == null)
+		if(!(entity instanceof PlayerEntity player))
 			return;
 
 		Hack hack = WurstClient.INSTANCE.getHax().getHackByName("CordTags");
 		if(!(hack instanceof CordTagsHack cordTags) || !cordTags.isEnabled())
 			return;
 
-		setNameTag(state, cordTags.addCoord(player, nameTag.copy()));
-	}
-
-	private Text getNameTag(S state)
-	{
-		Field field = getNameField(state);
-		if(field == null)
-			return null;
-
-		try
-		{
-			return (Text)field.get(state);
-		}catch(ReflectiveOperationException e)
-		{
-			return null;
-		}
-	}
-
-	private void setNameTag(S state, Text value)
-	{
-		Field field = getNameField(state);
-		if(field == null)
-			return;
-
-		try
-		{
-			field.set(state, value);
-		}catch(ReflectiveOperationException e)
-		{
-			// Ignore if this version stores the name in a different way.
-		}
-	}
-
-	private Field getNameField(S state)
-	{
-		if(cachedNameField != null)
-			return cachedNameField;
-
-		Class<?> clazz = state.getClass();
-		for(String candidate : NAME_FIELD_CANDIDATES)
-		{
-			try
-			{
-				Field field = clazz.getField(candidate);
-				if(Text.class.isAssignableFrom(field.getType()))
-				{
-					cachedNameField = field;
-					return field;
-				}
-			}catch(NoSuchFieldException e)
-			{
-				// Try next candidate name.
-			}
-		}
-
-		for(Field field : clazz.getFields())
-			if(Text.class.isAssignableFrom(field.getType()))
-			{
-				cachedNameField = field;
-				return field;
-			}
-
-		return null;
+		Text nameTag = accessor.koboshaddon$getNameTag();
+		accessor.koboshaddon$setNameTag(cordTags.addCoord(player, nameTag.copy()));
 	}
 }
